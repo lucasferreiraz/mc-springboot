b9a35debdf2abb18c81e35d35c2806537e9e9e20
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-04-27 22:12:28 -0300

    Definindo as entidades Estado e Cidade e o seu relacionamento

--- src/main/java/io/lucasprojects/mcspringboot/McSpringbootApplication.java
+++ src/main/java/io/lucasprojects/mcspringboot/McSpringbootApplication.java
@@ -1,10 +1,19 @@
 package io.lucasprojects.mcspringboot;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.lucasprojects.mcspringboot.domain.Categoria;
+import io.lucasprojects.mcspringboot.domain.Cidade;
+import io.lucasprojects.mcspringboot.domain.Estado;
+import io.lucasprojects.mcspringboot.domain.Produto;
 import io.lucasprojects.mcspringboot.repositories.CategoriaRepository;
+import io.lucasprojects.mcspringboot.repositories.CidadeRepository;
+import io.lucasprojects.mcspringboot.repositories.EstadoRepository;
 import io.lucasprojects.mcspringboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -15,15 +24,18 @@ public class McSpringbootApplication /*implements CommandLineRunner*/ {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McSpringbootApplication.class, args);
 	}
 	
 	/*
-	 * 
-	 * Alimentei e fiz as relações de forma provisória no banco 
-	 * 
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -45,7 +57,20 @@ public class McSpringbootApplication /*implements CommandLineRunner*/ {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 	}
 	*/
-
 }
---
+++ src/main/java/io/lucasprojects/mcspringboot/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package io.lucasprojects.mcspringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.lucasprojects.mcspringboot.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
